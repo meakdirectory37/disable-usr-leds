13c1f9471b23a97c626cabd810c1347e7c263663
meakdirectory37 <minecrafthelp74@gmail.com>
2018-08-08 12:30:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-g++ -o turn-off-usr-leds main.cpp
-sudo mv turn-off-usr-leds /usr/bin/
+sudo g++ -o /usr/bin/turn-off-usr-leds main.cpp
 mv turn-off-usr-leds.service /lib/systemd/system/
 systemctl start turn-off-usr-leds.service
 systemctl enable turn-off-usr-leds.service
